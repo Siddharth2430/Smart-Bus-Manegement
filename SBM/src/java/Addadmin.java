0ac22e89771c18974d7152ad14e3188f5a8dda80
Siddharth2430 <44302419+Siddharth2430@users.noreply.github.com>
2020-01-31 23:44:51 +0530

    Update Addadmin.java

--- SBM/src/java/Addadmin.java
+++ SBM/src/java/Addadmin.java
@@ -1,8 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+*
+* Project Name: 	SMART BUS MANAGEMENT
+* Author List: 		SIDDHARTH SHRIVASTAVA, RAJESHWARI KAUSHAL
+* Filename: 		Addadmin.java
+* Functions: 		init(),processRequest()
+* Global Variables:	
+*
+*/
 
 import connection.ConnectionDB;
 import java.io.IOException;
